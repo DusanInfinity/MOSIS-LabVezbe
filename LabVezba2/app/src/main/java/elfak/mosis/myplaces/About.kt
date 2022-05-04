d225e6395bea0c554f61c61840f00a625620cb60
stefandobrosavljevic <stefipanter999@gmail.com>
2022-05-04 21:14:37 +0200

    Dodata lista my places i dinamicki itemi kroz kotlin kod

--- LabVezba2/app/src/main/java/elfak/mosis/myplaces/About.kt
+++ LabVezba2/app/src/main/java/elfak/mosis/myplaces/About.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ListView
 
 class About : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
